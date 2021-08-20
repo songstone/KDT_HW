e23d32afb2fb1e450131fced83aaf8a9b47ebe56
songstone <thdtmdgus5505@gmail.com>
2021-08-20 15:45:55 +0900

    fix: bug & typo fix
    - refactor js code to output alert when click the submit button
    - fix typo

--- blog-structure/assets/js/main.js
+++ blog-structure/assets/js/main.js
@@ -1,7 +1,11 @@
 // 글쓰기 버튼 페이지 이동 방지
-const $submit = document.querySelector('button[type="submit"]');
-$submit &&
-    $submit.addEventListener("click", e => {
-        e.preventDefault();
-        alert("Great!");
-    });
+document.addEventListener("DOMContentLoaded",
+function(){
+    const $submit = document.querySelector('button[type="submit"]');
+    $submit &&
+        $submit.addEventListener("click", e => {
+            e.preventDefault();
+            alert("Great!");
+        });
+});
+
